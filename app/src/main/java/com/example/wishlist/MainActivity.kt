f64539b17e19fad98c9330c190852b2002df92ab
yannelly25 <yannellym@gmail.com>
2023-03-05 13:58:39 -0500

    recyclerview updates

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -8,40 +8,59 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var submitButton: Button
     private lateinit var recyclerView: RecyclerView
+    private lateinit var itemNameView: TextView
+    private lateinit var itemPriceView: TextView
+    private lateinit var itemLocationView: TextView
+
 
     private val wishlistItems = mutableListOf<WishlistItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        // finds the recyclerview and submit button by ID
         recyclerView = findViewById(R.id.recycler_View)
         submitButton = findViewById(R.id.submit_button)
+        itemNameView = findViewById(R.id.itemNameView)
+        itemPriceView = findViewById(R.id.itemPriceView)
+        itemLocationView= findViewById(R.id.itemLocationView)
+
+        // Sets the recyclerView layout manager to this
         recyclerView.layoutManager = LinearLayoutManager(this)
-
+        // sets the recyclerview's adapter
         recyclerView.adapter = RecyclerViewAdapter(wishlistItems)
-
+        // sets a click listener on the submit button
         submitButton.setOnClickListener {
+            // calls the func to add the item to the wish list
             addItemToWishlist()
         }
-
     }
 
+    // func to add items to the list
     private fun addItemToWishlist() {
-        val newItemName = "name of prod"
-        val newPrice = "price of prod"
-        val newLocation = "loc   of prod"
-//        println(newItemName.toString())
+        // get the item's name, price, location
+        val newItemName = findViewById<EditText>(R.id.itemNameView).text.toString()
+        val newPrice = findViewById<EditText>(R.id.itemPriceView).text.toString()
+        val newLocation = findViewById<EditText>(R.id.itemLocationView).text.toString()
+        // create a variable that holds the three variables above
         val newItem = WishlistItem(newItemName, newPrice, newLocation)
+        // add the newItem created to the wishlistitems mutable list
         wishlistItems.add(newItem)
-        println(wishlistItems)
-        recyclerView.adapter?.notifyItemInserted(wishlistItems.size - 1)
-    }
+        // notifiy the recyclerview that an item has been inserted. Insert at position size of the wishlist - 1
+        val newItemIndex = wishlistItems.size - 1
+        recyclerView.adapter?.notifyItemInserted(newItemIndex)
+        // log to see if items have been added to the wishlistitems mutable list
+        Log.d("WISHLIST", wishlistItems.toString())
 
+        // clear the input fields
+        itemNameView.text = ""
+        itemPriceView.text = ""
+        itemLocationView.text = ""
+    }
 }
--- app/src/main/java/com/example/wishlist/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/wishlist/RecyclerViewAdapter.kt
@@ -3,28 +3,37 @@ package com.example.wishlist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerViewAdapter(private val wishlistItems: List<WishlistItem>) :
+class RecyclerViewAdapter(private val wishlistItems: MutableList<WishlistItem>) :
     RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val itemNameTextView: TextView = itemView.findViewById(R.id.itemName)
+        val itemNameTextView: TextView = itemView.findViewById(R.id.itemNameView)
+        val itemPriceTextView: TextView  = itemView.findViewById(R.id.itemPriceView)
+        val itemLocationTextView: TextView  = itemView.findViewById(R.id.itemLocationView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.wishlist_item, parent, false)
-//        println(itemView)
+
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val itemView = inflater.inflate(R.layout.wishlist_item, parent, false)
+        // Return a new holder instance
         return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val currentItem = wishlistItems[position]
-        holder.itemNameTextView.text = currentItem.name
+        // gets the current item from the wishlistitems mutable list
+        val item = wishlistItems[position]
+        // Set item views based on views and data model
+        holder.itemNameTextView.text = item.name
+        holder.itemPriceTextView.text = item.price
+        holder.itemLocationTextView.text = item.location
     }
-
+    // func to get the size of the wishlistitems mutable list
     override fun getItemCount() = wishlistItems.size
 }
-
