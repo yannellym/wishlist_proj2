fa6c78313e7f6616c0151969cbdb4922807e18da
yannelly25 <yannellym@gmail.com>
2023-03-05 14:19:44 -0500

    all features working

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -14,9 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var submitButton: Button
     private lateinit var recyclerView: RecyclerView
-    private lateinit var itemNameView: TextView
-    private lateinit var itemPriceView: TextView
-    private lateinit var itemLocationView: TextView
+    private lateinit var itemNameView: EditText
+    private lateinit var itemPriceView: EditText
+    private lateinit var itemLocationView: EditText
 
 
     private val wishlistItems = mutableListOf<WishlistItem>()
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         submitButton = findViewById(R.id.submit_button)
         itemNameView = findViewById(R.id.itemNameView)
         itemPriceView = findViewById(R.id.itemPriceView)
-        itemLocationView= findViewById(R.id.itemLocationView)
+        itemLocationView = findViewById(R.id.itemLocationView)
 
         // Sets the recyclerView layout manager to this
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -44,23 +44,19 @@ class MainActivity : AppCompatActivity() {
 
     // func to add items to the list
     private fun addItemToWishlist() {
-        // get the item's name, price, location
-        val newItemName = findViewById<EditText>(R.id.itemNameView).text.toString()
-        val newPrice = findViewById<EditText>(R.id.itemPriceView).text.toString()
-        val newLocation = findViewById<EditText>(R.id.itemLocationView).text.toString()
-        // create a variable that holds the three variables above
+        val newItemName = itemNameView.text.toString()
+        val newPrice = itemPriceView.text.toString()
+        val newLocation = itemLocationView.text.toString()
         val newItem = WishlistItem(newItemName, newPrice, newLocation)
-        // add the newItem created to the wishlistitems mutable list
+
         wishlistItems.add(newItem)
-        // notifiy the recyclerview that an item has been inserted. Insert at position size of the wishlist - 1
         val newItemIndex = wishlistItems.size - 1
         recyclerView.adapter?.notifyItemInserted(newItemIndex)
-        // log to see if items have been added to the wishlistitems mutable list
+
         Log.d("WISHLIST", wishlistItems.toString())
 
-        // clear the input fields
-        itemNameView.text = ""
-        itemPriceView.text = ""
-        itemLocationView.text = ""
+        itemNameView.setText("")
+        itemPriceView.setText("")
+        itemLocationView.setText("")
     }
 }
